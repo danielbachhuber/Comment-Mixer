365f27286b8626dfaa5b237ad357c2ea5c7af6ee
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-02 12:35:05 -0400

    New settings class

---
+++ php/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+if ( !class_exists( 'comment_mixer_settings' ) ){
+  
+/**
+ * Class for managing all Comment Mixer settings views
+ */
+class comment_mixer_settings
+  {
+  
+    function __construct() {
+      
+    }
+
+	/**
+	 * Initialize the plugin settings
+	 * @todo Do this
+	 */
+	function init() {
+		
+	}
+
+}
+
+
+?>
