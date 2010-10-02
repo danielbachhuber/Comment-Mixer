68a51c5477278173a214cf47a8446cf7eab1c82d
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-02 12:40:52 -0400

    Shell for taxonomies class

---
+++ php/taxonomies.php
@@ -0,0 +1,26 @@
+<?php
+
+if ( !class_exists( 'comment_mixer_taxonomies' ) ){
+  
+/**
+ * Class for managing all Comment Mixer taxonomy methods
+ */
+class comment_mixer_taxonomies
+  {
+  
+    function __construct() {
+      
+    }
+
+	/**
+	 * Initialize the plugin taxonomies
+	 * @todo Do this
+	 */
+	function init() {
+		
+	}
+
+}
+
+
+?>
